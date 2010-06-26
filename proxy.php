b001a72409470da1784b9ff55e881576eb4e81e1
Tom Insam <tom@jerakeen.org>
2010-06-26 18:39:49 +0000

    fix proxy

--- proxy.php
+++ proxy.php
@@ -9,26 +9,38 @@ $command = $_SERVER['PATH_INFO']; # isset($_REQUEST['command']) && !empty($_REQU
 #  exit();
 #endif;
 
-$url = $urlbase.$command."?".$_SERVER["QUERY_STRING"];
+$url = $urlbase.$command;
+if ($_SERVER["QUERY_STRING"]) {
+  $url = $url."?".$_SERVER["QUERY_STRING"];
+}
+
+error_log("===");
+error_log($url);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); # don't follow 301/302
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $postVars = array();;
-  foreach ($_POST as $k => $v) {
-    $postVars[] = $k . "=" . $v;
-  }
-  $postVars = implode("&", $postVars);
+  $postData = file_get_contents('php://input');
+  
+  #$postVars = array();;
+  #foreach ($_POST as $k => $v) {
+  #  error_log("POSTING " . $k . " = " . $v);
+  #  $postVars[] = $k . "=" . $v;
+  #}
+  #$postVars = implode("&", $postVars);
   curl_setopt ($ch, CURLOPT_POST, true);
-  curl_setopt ($ch, CURLOPT_POSTFIELDS, $postVars);
+  error_log($postData);
+  curl_setopt ($ch, CURLOPT_POSTFIELDS, $postData);
 }
+error_log("---");
 
 curl_setopt($ch, CURLOPT_URL, $url);
 $data = curl_exec($ch);
 $curl_info = curl_getinfo($ch);
 
 header($curl_info['http_code'], true, $curl_info['http_code']);
+error_log($data);
 print_r($data);
 curl_close($ch);
